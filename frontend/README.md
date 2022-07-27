8d45f959f174d09006801a048a1f17604eab58f6
RustingChen <bc39@rice.edu>
2022-07-27 15:16:04 -0500

    upload

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+netid: bc39,   
+frontend: https://volatile-reason.surge.sh/,   
+backend: https://pagebackend.herokuapp.com,  
+test username: testUser  
+password：123  
+
+I want to use 2 slip days for the final app. Thank you！
---
+++ frontend/src/components/main/main.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Headline from './headline'
+import Following from './following'
+import ArticlesView from '../article/articlesView'
+
+const Main = () => (
+    <div className="row">
+        <div className="row">&nbsp;</div>
+        <div className="row">&nbsp;</div>
+        <div className="row">&nbsp;</div>
+
+        <div className="col-md-3 col-xs-3" id="contain-setting">
+            <Headline/>
+            <Following/>
+        </div>
+        <div className="col-md-9 col-xs-9">
+        <ArticlesView/>
+        </div>
+    </div>
+)
+
+export default Main
+
+
+
+/** WEBPACK FOOTER **
+ ** ./src/components/main/main.js
+ **/
